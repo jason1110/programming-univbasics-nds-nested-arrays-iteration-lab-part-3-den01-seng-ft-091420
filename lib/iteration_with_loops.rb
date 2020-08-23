b0d850f0f4b244c406b0d092141438b4c7b68adf
Jason Easterly <jason.easterly11@gmail.com>
2020-08-23 19:09:33 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -10,7 +10,7 @@ count = 0
   each_string = ""
     while inner_count < src[count].length do
       if src[count][inner_count].class == String
-        new_string = src[count][inner_count]
+        each_string = src[count][inner_count]
       end  
     inner_count +=1
   end
